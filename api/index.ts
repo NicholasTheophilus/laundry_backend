5ee1e76737ca4fb2cc667a7f2e9289bddacfc552
unknown <farizabqarillahi@gmail.com>
2024-06-17 01:11:23 +0700

    first commit

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,42 @@
-// index.js
+// // index.js
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const models = require('../models');
+// const laundriesRoutes = require('../routes/laundries');
+// const migrationsRoutes = require('../routes/migrations');
+// const ordersRoutes = require('../routes/orders');
+// const servicesRoutes = require('../routes/services');
+// const usersRoutes = require('../routes/users');
+
+// app.get("/", (_req: any, res: { send: (arg0: string) => any; }) => res.send("Express on Vercel"));
+
+// app.listen(process.env.PORT, () => console.log("Server ready on port 3000."));
+
+
+
+// app.use(express.json());
+
+// app.use('/api/laundries', laundriesRoutes);
+// app.use('/api/migrations', migrationsRoutes);
+// app.use('/api/orders', ordersRoutes);
+// app.use('/api/services', servicesRoutes);
+// app.use('/api/users', usersRoutes);
+// app.use('/api/users/login', usersRoutes);
+
+// models.sequelize.sync().then(() => {
+//   app.listen(process.env.PORT, () => {
+//     console.log('Server is running on port 5000');
+//   });
+// }).catch((error) => {
+//   console.error('Unable to sync the database:', error);
+// });
+
+// module.exports = app;
+
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const serverless = require('serverless-http'); // Modul untuk serverless
 const models = require('../models');
 const laundriesRoutes = require('../routes/laundries');
 const migrationsRoutes = require('../routes/migrations');
@@ -9,11 +44,7 @@ const ordersRoutes = require('../routes/orders');
 const servicesRoutes = require('../routes/services');
 const usersRoutes = require('../routes/users');
 
-app.get("/", (_req: any, res: { send: (arg0: string) => any; }) => res.send("Express on Vercel"));
-
-app.listen(process.env.PORT, () => console.log("Server ready on port 3000."));
-
-
+const app = express();
 
 app.use(express.json());
 
@@ -24,11 +55,10 @@ app.use('/api/services', servicesRoutes);
 app.use('/api/users', usersRoutes);
 
 models.sequelize.sync().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log('Server is running on port 5000');
-  });
+  console.log('Database sync successful.');
+
+  // Tambahkan kode ini untuk deploy ke serverless function
+  module.exports.handler = serverless(app);
 }).catch((error) => {
   console.error('Unable to sync the database:', error);
 });
-
-module.exports = app;
