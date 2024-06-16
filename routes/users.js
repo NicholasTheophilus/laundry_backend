62f47bddd124526cc726f81780e578f988bd7e7e
unknown <farizabqarillahi@gmail.com>
2024-06-17 02:27:04 +0700

    first commit

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
+router.post('/login', usersController.loginUser);
 router.get('/', usersController.getAllUsers);
 router.get('/:id', usersController.getUserById);
 router.post('/', usersController.createUser);
-router.post('/login', usersController.loginUser);
 router.put('/:id', usersController.updateUser);
 router.delete('/:id', usersController.deleteUser);
 
