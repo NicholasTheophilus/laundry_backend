b699456c7e4cff5bbd9150f13219217171a342d2
unknown <farizabqarillahi@gmail.com>
2024-06-17 05:02:05 +0700

    first commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -29,15 +29,15 @@ exports.getUserById = async (req, res) => {
 
 exports.createUser = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { email, phone, password } = req.body;
 
     // Hash password menggunakan bcrypt
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Membuat user baru dengan password yang telah di-hash
     const user = await User.create({
-      name,
       email,
+      phone,
       password: hashedPassword,
     });
 
